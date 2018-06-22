8780a8d0f2c1e0025d8baf5f8a3970ad106e724b
Alex Delgado <delgadoalex@icloud.com>
2018-06-21 22:39:47 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work.

--- js/app.js
+++ js/app.js
@@ -12,15 +12,32 @@ $(() => {
 
   const $div = $("<div>");
   $div.attr("id", "container"); 
+  
   const $h1 = $('<h1>');
   $h1.text('Hogwarts');
   $('body').append($h1); 
- 
+  
+  const $h2 = $('<h2>'); 
+  $h2.text('Alex Delgado'); 
+  $('body').append($h2);
 
+  const $h3 = $('<h3>')
+  $h3.text('Gryffindor'); 
+  $('body').append($h3); 
+
+  const $h4 = $('<h4></h4>');
+  $h4.text('Seymour');
+  $h4.addClass('tiger');
+  $('body').append($h4);
+
+  $('body').append('<h4>Hornthowe Wand with Dragon Heartstring Core</h4>');
+  
 
 });
 
- console.log('container');
+
+ // console.log('container');
+
 
 
 /// YEAR 1 /// 
