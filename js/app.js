24843ad5be78128d3c0d92d6020fd0b6749ff79d
Alex Delgado <delgadoalex@icloud.com>
2018-06-21 21:41:56 -0500

    I added my first element and it was fun

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-console.log('JS is working'); 
+// console.log('JS is working'); 
 
-if (typeof jQuery == 'undefined'){
-  console.log('oops! I still have to link my jQuery properly!');
-} else {
-  console.log('I did it! I linked jQuery and this js file!')
-};
+// if (typeof jQuery == 'undefined'){
+//   console.log('oops! I still have to link my jQuery properly!');
+// } else {
+//   console.log('I did it! I linked jQuery and this js file!')
+// };
+
+ /// YEAR 1 /// 
+
+$(() => {
+
+  const $div = $("<div>");
+  $div.attr("id", "container"); 
+  const $h1 = $('<h1>');
+  $h1.text('Hogwarts');
+  $('body').append($h1); 
+ 
+
+
+});
+
+ console.log('container');
+
+
+/// YEAR 1 /// 
 
