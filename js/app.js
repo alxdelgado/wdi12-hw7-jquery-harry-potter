1afc69768f3c23619d1eb7fbed3c488e487d6de8
Alex Delgado <delgadoalex@icloud.com>
2018-06-21 20:33:53 -0500

    Commit 0 - all my files are linked and loaded

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log('JS is working'); 
+
+if (typeof jQuery == 'undefined'){
+  console.log('oops! I still have to link my jQuery properly!');
+} else {
+  console.log('I did it! I linked jQuery and this js file!')
+};
+
