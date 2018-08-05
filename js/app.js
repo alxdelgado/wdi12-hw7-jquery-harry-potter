34dba4dc5091f51e5ced21bb2aa893e295938666
Alex Delgado <delgadoalex@icloud.com>
2018-08-04 22:12:08 -0500

    Commit 1 - I added mt first element and it was fun

--- js/app.js
+++ js/app.js
@@ -6,7 +6,19 @@ if (typeof jQuery == 'undefined'){
   // console.log('I did it! I linked jQuery and this js file!');
 };
 
-const $body = $('body'); 
-const $container = $('#container')
+
+$(() => {
+  const $body = $('body'); 
+  const $container = $('#container');
+
+  // Year 1 // 
+  const $h1 = $('<h1></h1>');
+  console.log($h1);
+  $h1.text('Hogwarts');  
+  $body.append($h1);
+  
+
+});
+
 
 
