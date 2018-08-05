90c019da2f4f0882c173fb5d6efb749c0a729a66
Alex Delgado <delgadoalex@icloud.com>
2018-08-04 22:28:03 -0500

    Commit 2 - I added more elements. My power grow with my dedication and hard work

--- js/app.js
+++ js/app.js
@@ -16,7 +16,27 @@ $(() => {
   console.log($h1);
   $h1.text('Hogwarts');  
   $body.append($h1);
-  
+
+  // Year 2 // 
+  const $h2 = $('<h2></h2>');
+  $h2.text('Alex');
+  $body.append($h2);
+
+  const $h3 = $('<h3></h3>'); 
+  $h3.text('Gryffindor');
+  $body.append($h3); 
+
+  const $h4 = $('<h4></h4>'); 
+  $h4.text('Seymour'); 
+  $h4.addClass('Owl'); 
+  $body.append($h4); 
+
+  const $h4_2 = $('<h4></h4>'); 
+  $h4_2.text('I do not know anything about Harry Potter');
+  $body.append($h4_2);
+
+   
+
 
 });
 
