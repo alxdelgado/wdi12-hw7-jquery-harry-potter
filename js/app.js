a825c607e439ec5a91b716151255120e920c2b05
Alex Delgado <delgadoalex@icloud.com>
2018-08-04 21:57:08 -0500

    Commit 0 - All my files are linked and loaded

--- js/app.js
+++ js/app.js
@@ -1,55 +1,12 @@
 // console.log('JS is working'); 
 
-// if (typeof jQuery == 'undefined'){
-//   console.log('oops! I still have to link my jQuery properly!');
-// } else {
-//   console.log('I did it! I linked jQuery and this js file!')
-// };
+if (typeof jQuery == 'undefined'){
+  // console.log('oops! I still have to link my jQuery properly!');
+} else {
+  // console.log('I did it! I linked jQuery and this js file!');
+};
 
- /// YEAR 1 /// 
+const $body = $('body'); 
+const $container = $('#container')
 
-$(() => {
-
-  const $div = $("<div>");
-  $div.attr("id", "container"); 
-
-  const $container = $('#container');
-
-  // const $body = $('body');
-  
-  const $h1 = $('<h1>');
-  $h1.text('Hogwarts');
-  $('body').append($h1); 
-  
-  const $h2 = $('<h2>'); 
-  $h2.text('Alex Delgado'); 
-  $('body').append($h2);
-
-  const $h3 = $('<h3>')
-  $h3.text('Gryffindor'); 
-  $('body').append($h3); 
-
-  const $h4 = $('<h4></h4>');
-  $h4.text('Seymour');
-  $h4.addClass('tiger');
-  $('body').append($h4);
-
-  $('body').append('<h4>Hornthowe Wand with Dragon Heartstring Core</h4>');
-
-  const $li = $('<li/>');
-  $li.text('butter beer')
-  $ul = $('ul');
-  $('body').append($li);
-  $('body').append('<li>invisibility cloak</li>')
-
-  
-
-});
-
-
- // console.log('container');
-
-
-
-/// YEAR 1 /// 
 
